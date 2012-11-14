bc3c96df408e93446ca81ba8fa99175b608db159
Ben Evans <benny.j.evans92@gmail.com>
2012-11-15 01:29:03 +1000

    Connections working.

--- node.py
+++ node.py
@@ -5,6 +5,8 @@ from threading import Thread
 import signal
 import sys
 
+####################### Globals #######################
+
 #Networking
 nodeCtrlPort = 7228
 nodeRelayPort = 7229
@@ -12,62 +14,86 @@ nodeAddr = ""
 maxConnections = 40
 servCtrl = None
 servRelay = None
+MAX_REC_SIZE = 1024
 
+#Node
 nodeID = ""
 fingerTable = [0] * 160
 
-#Signal Handlers
+############### Signal Handlers and Exit ###############
+
 def exit_signal_handler(signal, frame):
-    try:
-        close(servCtrl)
-    except:
-        pass
-    try:
-        close(servRelay)
-    except:
-        pass 
-    sys.exit(0)
+    print "Closing Connections"
+    graceful_exit(0)
     
 signal.signal(signal.SIGINT, exit_signal_handler)
 
+def graceful_exit(exitCode):
+    try:
+        servCtrl.close()
+    except:
+        pass
+    try:
+        servRelay.close()
+    except:
+        pass
+    exit(exitCode)
+
+#################### Network Sockets ####################
+    
+def handle_ctrl_connection(conn, addr):
+    data = conn.recv(MAX_REC_SIZE) 
+    if data: 
+        conn.send(data) 
+    conn.close()
+
+def handle_connection(conn, addr):
+    data = conn.recv(MAX_REC_SIZE) 
+    if data: 
+        conn.send(data) 
+    conn.close()
+
 def wait_for_ctrl_connections():
+    global servCtrl
     servCtrl = socket(AF_INET, SOCK_STREAM)
     ADDR = (nodeAddr, nodeCtrlPort)
     servCtrl.bind((ADDR))
     servCtrl.listen(maxConnections) 
-    print "Now waiting\n"
-    sys.stdout.flush()
+    print "Waiting for Control Connections."
     while 1:
         conn, addr = servCtrl.accept() #accept the connection
-        conn.send('TEST')
-        conn.close()
+        t = Thread(target=handle_ctrl_connection, args=(conn, addr))
+        t.start()
 
 def wait_for_connections():
+    global servRelay
     servRelay = socket(AF_INET, SOCK_STREAM)
     ADDR = (nodeAddr, nodeRelayPort)
     servRelay.bind((ADDR))
     servRelay.listen(maxConnections) 
-    print "Now waiting\n"
-    sys.stdout.flush()
+    print "Waiting for Relay Connections."
     while 1:
-        conn,addr = serv.accept() #accept the connection
-        conn.send('TEST')
-        conn.close()
+        conn, addr = servRelay.accept() #accept the connection
+        t = Thread(target=handle_connection, args=(conn, addr))
+        t.start()
 
 
-
-print hash_str("ben0")
-
-
-
+######################### Main #########################
 def main():
-    print "Here"
-    t = Thread(target=wait_for_ctrl_connections)
-    t.start()
-    t1 = Thread(target=wait_for_connections)
-    t1.start()
-    t.join()
-    t1.join()
+    print hash_str("test")
+    try:
+        listenCtrlThread = Thread(target=wait_for_ctrl_connections)
+        listenCtrlThread.daemon = True
+        listenCtrlThread.start()
+        listenThread = Thread(target=wait_for_connections)
+        listenThread.daemon = True
+        listenThread.start()
+        while 1:
+            #The threads should never die
+            listenCtrlThread.join(1)
+            listenThread.join(1)
+    except (KeyboardInterrupt, SystemExit):
+        graceful_exit(0)
 
 if __name__ == "__main__":
     main()
