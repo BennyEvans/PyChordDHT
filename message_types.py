4c5e6709e6f962450eca88d80408c63dfdee6ab8
Ben Evans <benny.j.evans92@gmail.com>
2012-11-15 18:30:35 +1000

    Started the implementation of node structure functions.

---
+++ message_types.py
@@ -0,0 +1,12 @@
+#Message Types
+
+class ControlMessageTypes():
+    GET_NEXT_NODE = 0
+    GET_ROOT_NODE = 1
+    IS_SUCCESSOR = 2
+    IS_PREDECESSOR = 3
+    GET_PREDECESSOR = 4
+    UPDATE_FINGER_TABLE = 5
+    MOVE_KEYS = 6
+    PING_NODE = 7
+    
